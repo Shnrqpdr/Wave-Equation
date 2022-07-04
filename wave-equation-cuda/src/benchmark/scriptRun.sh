df513daa20dda9efe6c986c2856a009f7207d23b
Pedro <hnrqpdr.sc@gmail.com>
2022-07-04 20:39:23 -0300

    Preparando script

--- wave-equation-cuda/src/benchmark/scriptRun.sh
+++ wave-equation-cuda/src/benchmark/scriptRun.sh
@@ -2,32 +2,35 @@
 
 for j in 896 1792 2688 3584;
 do
-    mkdir $j
+    mkdir "malha$j"
 
-    cd $j
+    cd "malha$j"
 
-    cp ../wave-aux.cu .
+    cp ../wave.cu .
 
-    sed "s/XYXY/$j/g" wave-aux.cu > wave.cu
+    sed "s/XYXY/$j/g" wave.cu > exec.cu
 
     echo "Iniciando o cálculo do programa sem as flags"
 
-    nvcc -o wave.x wave.cu
+    nvcc -o wave.x exec.cu
 
-    ( time ./wave.x ) 2> noFlags.out
+    ( time ./wave.x ) 2> wave_noFlags.dat
 
     echo "Iniciando o cálculo do programa com as flags"
 
     for i in $(seq 0 3)
         do 
-            nvcc -O$i -o wave_O$i.x wave.cu
-            ( time ./wave_O$i.x ) 2> wave_O$i.out
+            echo "Usando as flags -O$i"
+            nvcc -O$i -o wave_O$i.x exec.cu
+            ( time ./wave_O$i.x ) 2> wave_O$i.dat
 
-            nvcc -use_fast_math -O$i -o wave_fast_O$i.x wave.cu
-            ( time ./wave_fast_O$i.x ) 2> wave_fast_O$i.out
+            echo "Usando as flags -use_fast_math -O$i"
+            nvcc -use_fast_math -O$i -o wave_fast_O$i.x exec.cu
+            ( time ./wave_fast_O$i.x ) 2> wave_fast_O$i.dat
 
-            nvcc -Xptxas -O$i, -o wave_G_O$i.x wave.cu
-            ( time ./wave_G_O$i.x ) 2> wave_G_O$i.out
+            echo "Usando as flags -Xptxas -O$i"
+            nvcc -Xptxas -O$i, -o wave_G_O$i.x exec.cu
+            ( time ./wave_G_O$i.x ) 2> wave_G_O$i.dat
 
         done
 
--- wave-equation-cuda/src/benchmark/wave.cu
+++ wave-equation-cuda/src/benchmark/wave.cu
@@ -14,8 +14,8 @@
 #define xFinal 500.0
 #define yInicial 0
 #define yFinal 500.0
-#define	alpha 0.4
-#define	gamma 0.8
+#define alpha 0.4
+#define gamma 0.8
 
 #define CHECK(call)                                                \
     {                                                              \
@@ -27,86 +27,6 @@
                     cudaGetErrorString(error));                    \
         }                                                          \
     }
-
-__global__ void waveEquationKernel(double *wave, double *waveFuture, double *wavePast)
-{
-    int i = blockIdx.x;
-    int j = threadIdx.x;
-
-    if ((i > 0 && i < N - 1) && (j > 0 && j < N - 1))
-        waveFuture[i * blockDim.x + j] = 2 * wave[i * blockDim.x + j] * (1 - alpha * alpha - gamma * gamma) - wavePast[i * blockDim.x + j] + alpha * alpha * wave[(i + 1) * blockDim.x + j] + alpha * alpha * wave[(i - 1) * blockDim.x + j] + gamma * gamma * wave[i * blockDim.x + (j + 1)] + gamma * gamma * wave[i * blockDim.x + (j - 1)];
-}
-
-void writeFiles(double *wave, double dx, double dy)
-{
-    int i, j;
-    FILE *fileStaticPlot;
-
-    fileStaticPlot = fopen("WaveStatic2.dat", "w");
-
-    fprintf(fileStaticPlot, "x\ty\tf\n");
-
-    for (i = 1; i < N - 1; i++)
-    {
-        for (j = 1; j < N - 1; j++)
-        {
-            fprintf(fileStaticPlot, "%lf\t%lf\t%lf\n", i * dx, j * dy, wave[i * N + j]);
-        }
-    }
-
-    fclose(fileStaticPlot);
-}
-
-void initialCondition(double *wave)
-{
-    int i, j;
-    for (j = 0; j < N; j++)
-    {
-        for (i = 0; i < N; i++)
-        {
-            wave[i * N + j] = 4 * sin(M_PI * i / 75.0);
-        }
-    }
-}
-
-void derivativeCondition(double *wave, double *wavePast){
-
-	int i, j;
-
-	for (i = 1; i < N-1; i++)
-	{
-		for (j = 1; j < N-1; j++)
-		{
-			wave[i*N+j] = (2*wavePast[i*N + j]*(1 - alpha*alpha - gamma*gamma) + alpha*alpha*wavePast[(i+1)*N + j] + alpha*alpha*wavePast[(i-1)*N + j] + gamma*gamma*wavePast[i*N + (j+1)] +  gamma*gamma*wavePast[i*N + (j-1)])/2;
-		}
-	}
-}
-
-double resultsValidation(double *wave, double dx, double dy){
-
-    int i, j;
-    double erro, maiorErro, analitica;
-
-    for(i = 0; i < N; i++){
-        for(j = 0; j < N; j ++){
-
-            analitica = sin((j*M_PI*dx)/xFinal)*sin((i*M_PI*dy)/yFinal)*(-(2000/((M_PI)*(M_PI)))*((sin(M_PI*j)*(cos(M_PI*i) - 1))/((j*j - 250000)*i)))*cos(sqrt(mu/T)*(sqrt((j*M_PI/xFinal)*(j*M_PI/xFinal) + (i*M_PI/yFinal)*(i*M_PI/yFinal)))*tempoTotal);
-
-            erro = fabs((wave[i*N + j] - analitica)/analitica);
-
-            if(i == 0 && j == 0){
-                maiorErro = erro;
-            }
-            else{
-                if(erro > maiorErro){
-                    maiorErro = erro;
-                }
-            }
-        }
-    }
-
-    return maiorErro;
-}
 
 void deviceCapabilities()
 {
@@ -154,37 +74,129 @@ void deviceCapabilities()
     }
 }
 
-void actionWork(double dx, double dy){
+__global__ void finiteDifferenceKernel(double *wave, double *waveFuture, double *wavePast)
+{
+    unsigned int i = blockIdx.x;
+    unsigned int j = threadIdx.x;
+
+    if ((i > 0 && i < N - 1) && (j > 0 && j < N - 1))
+        waveFuture[i * blockDim.x + j] = 2 * wave[i * blockDim.x + j] * (1 - alpha * alpha - gamma * gamma) - wavePast[i * blockDim.x + j] + alpha * alpha * wave[(i + 1) * blockDim.x + j] + alpha * alpha * wave[(i - 1) * blockDim.x + j] + gamma * gamma * wave[i * blockDim.x + (j + 1)] + gamma * gamma * wave[i * blockDim.x + (j - 1)];
+}
+
+void writeFiles(double *wave, double dx, double dy)
+{
+    int i, j;
+    FILE *fileStaticPlot;
+
+    fileStaticPlot = fopen("WaveStatic2.dat", "w");
+
+    fprintf(fileStaticPlot, "x\ty\tf\n");
+
+    for (i = 1; i < N - 1; i++)
+    {
+        for (j = 1; j < N - 1; j++)
+        {
+            fprintf(fileStaticPlot, "%lf\t%lf\t%lf\n", i * dx, j * dy, wave[i * N + j]);
+        }
+    }
+
+    fclose(fileStaticPlot);
+}
+
+void initialCondition(double *wave)
+{
+    int i, j;
+    for (j = 0; j < N; j++)
+    {
+        for (i = 0; i < N; i++)
+        {
+            wave[i * N + j] = 4 * sin(M_PI * i / 75.0);
+        }
+    }
+}
+
+void derivativeCondition(double *wave, double *wavePast)
+{
+
+    int i, j;
+
+    for (i = 1; i < N - 1; i++)
+    {
+        for (j = 1; j < N - 1; j++)
+        {
+            wave[i * N + j] = (2 * wavePast[i * N + j] * (1 - alpha * alpha - gamma * gamma) + alpha * alpha * wavePast[(i + 1) * N + j] + alpha * alpha * wavePast[(i - 1) * N + j] + gamma * gamma * wavePast[i * N + (j + 1)] + gamma * gamma * wavePast[i * N + (j - 1)]) / 2;
+        }
+    }
+}
+
+double resultsValidation(double *wave, double dx, double dy)
+{
+
+    int i, j;
+    double erro, maiorErro, analitica;
+
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < N; j++)
+        {
+
+            analitica = sin((j * M_PI * dx) / xFinal) * sin((i * M_PI * dy) / yFinal) * (-(2000 / ((M_PI) * (M_PI))) * ((sin(M_PI * j) * (cos(M_PI * i) - 1)) / ((j * j - 250000) * i))) * cos(sqrt(mu / T) * (sqrt((j * M_PI / xFinal) * (j * M_PI / xFinal) + (i * M_PI / yFinal) * (i * M_PI / yFinal))) * tempoTotal);
+
+            erro = fabs((wave[i * N + j] - analitica) / analitica);
+
+            if (i == 0 && j == 0)
+            {
+                maiorErro = erro;
+            }
+            else
+            {
+                if (erro > maiorErro)
+                {
+                    maiorErro = erro;
+                }
+            }
+        }
+    }
+
+    return maiorErro;
+}
+
+void actionWork(double dx, double dy)
+{
 
     int i, j, t;
+    int nBlocks, nThreads;
+
+    nBlocks = N;
+    nThreads = N;
 
     double *hostWave, *hostWaveFuture, *hostWavePast;       // Host variables
     double *deviceWave, *deviceWaveFuture, *deviceWavePast; // Device Variables
-    //double erro;
+    // double erro;
 
-    //printf("Alocando memoria no host\n");
+    // printf("Alocando memoria no host\n");
     hostWave = (double *)calloc((N * N), sizeof(double));
     hostWaveFuture = (double *)calloc((N * N), sizeof(double));
     hostWavePast = (double *)calloc((N * N), sizeof(double));
 
-    //printf("Colocando condição inicial.\n");
+    // printf("Colocando condição inicial.\n");
     initialCondition(hostWavePast);
-    //printf("Colocando condição da derivada.\n"); 
+    // printf("Colocando condição da derivada.\n");
     derivativeCondition(hostWave, hostWavePast);
 
-    //printf("Alocando memoria no Device\n");
+    // printf("Alocando memoria no Device\n");
     CHECK(cudaMalloc(&deviceWave, (N * N) * sizeof(double)));
     CHECK(cudaMalloc(&deviceWaveFuture, (N * N) * sizeof(double)));
-    CHECK(cudaMalloc(&deviceWavePast, (N * N) * sizeof(double))); 
-    
-    //printf("Iniciando calculo da função de onda.\n");
+    CHECK(cudaMalloc(&deviceWavePast, (N * N) * sizeof(double)));
+
+    // printf("Iniciando calculo da função de onda.\n");
     for (t = 0; t < tempoTotal; t++)
     {
         CHECK(cudaMemcpy(deviceWave, hostWave, (N * N) * sizeof(double), cudaMemcpyHostToDevice));
         CHECK(cudaMemcpy(deviceWaveFuture, hostWaveFuture, (N * N) * sizeof(double), cudaMemcpyHostToDevice));
         CHECK(cudaMemcpy(deviceWavePast, hostWavePast, (N * N) * sizeof(double), cudaMemcpyHostToDevice));
 
-        waveEquationKernel<<<N, N>>>(deviceWave, deviceWaveFuture, deviceWavePast);
+        finiteDifferenceKernel<<<nBlocks, nThreads>>>(deviceWave, deviceWaveFuture, deviceWavePast);
 
         CHECK(cudaMemcpy(hostWave, deviceWave, (N * N) * sizeof(double), cudaMemcpyDeviceToHost));
         CHECK(cudaMemcpy(hostWaveFuture, deviceWaveFuture, (N * N) * sizeof(double), cudaMemcpyDeviceToHost));
@@ -200,14 +212,14 @@ void actionWork(double dx, double dy){
         }
     }
 
-    //printf("Escrevendo no arquivo o resultado do cálculo\n");
-    //writeFiles(hostWave, dx, dy);
+    // printf("Escrevendo no arquivo o resultado do cálculo\n");
+    // writeFiles(hostWave, dx, dy);
 
-    //erro = resultsValidation(hostWave, dx, dy);
+    // erro = resultsValidation(hostWave, dx, dy);
 
-    //printf("Erro da solução numérica: %lf\n", erro);
+    // printf("Erro da solução numérica: %lf\n", erro);
 
-    //printf("Liberando memoria no host e device ... \n");
+    // printf("Liberando memoria no host e device ... \n");
 
     free(hostWave);
     free(hostWaveFuture);
@@ -225,11 +237,11 @@ int main()
     dx = (xFinal - xInicial) / N;
     dy = (yFinal - yInicial) / N;
 
-    //deviceCapabilities();
+    // deviceCapabilities();
 
-    //printf("dx: %lf\tdy: %lf\n", dx, dy);
+    // printf("dx: %lf\tdy: %lf\n", dx, dy);
     printf("Malha: %d x %d\n", N, N);
-    //printf("Tempo total: %d\n", tempoTotal);
+    // printf("Tempo total: %d\n", tempoTotal);
 
     clock_t beginTime = clock();
 
